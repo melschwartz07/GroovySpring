b8b3a17521f5a10b68865d17d2189f83b17ff326
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-06-30 11:13:37 -0500

    init commit

---
+++ src/main/java/com/codeup/groovyspring/GroovySpringApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.groovyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroovySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/groovyspring/GroovySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.groovyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
